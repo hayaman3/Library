a78020e11645ec524e4bee5c5ef544e75f7f4f76
Kevin <kevin.haya@yahoo.com>
2022-08-06 23:48:59 +0800

    Refractor script.js removed comments from  old codes

--- script.js
+++ script.js
@@ -1,61 +1,40 @@
-const book = document.getElementById("books");
+const library = document.getElementById("library");
 const form = document.getElementById("form");
 const showForm = document.getElementById("show-form");
 const submit = document.getElementById("submit");
 
-// let myLibrary = [];
-let library = [["a1","b1",1],["a2","b2",2],["a3","b3",3]];
+let libraryArr = [["a1","b1",1],["a2","b2",2],["a3","b3",3]];
 
-function Book(title, author, numOfPages) {
+let book1 = new Book(...libraryArr[0])
+let book2 = new Book(...libraryArr[1])
+let book3 = new Book(...libraryArr[2])
+
+function Book(title, author, numOfPages){
   // the constructor...
   this.title = title;
   this.author = author;
   this.numOfPages = numOfPages;
 //   this.background = "#file"
 }
-
-// addBookToLibrary()
-// function addBookToLibrary() {
-//     for(i=0;i<myLibrary.length;i++){
-//     // Create a div node:
-//     const node = document.createElement("div");
-//     node.classList.add("card")
-
-//     // Create a text node:
-//     const textnode = document.createTextNode(myLibrary[i]);
-
-//     // Append the text node to the "li" node:
-//     node.appendChild(textnode);
-
-//     // Append the "li" node to the list:
-//     document.querySelector("main").appendChild(node);
-//     }
-// }
-
 addBookToLibrary()
-function addBookToLibrary() {
-    for(i=0;i<library.length;i++){
+function addBookToLibrary(){
+    for(i=0;i<libraryArr.length;i++){
     // Create a div node:
     const node = document.createElement("div");
     node.classList.add("card")
-    
     let textnode = "";
+    let keys = ["Author: ","Title: ","Number of Pages: "]
+    let textnode2 = "";
+    let textnode3 = "";
     // Create a text node:
-    for(j=0;j<library.length;j++){
-        textnode = document.createTextNode("author: "+library[i][j]);
-
-        // console.log(library[i][j])
-    }
-    // Append the text node to the "li" node:
-    node.appendChild(textnode);
-
-
-
-    // Append the "li" node to the list:
-    document.querySelector("main").appendChild(node);
+        for(j=0;j<libraryArr[i].length;j++){
+            textnode = document.createTextNode(keys[j]+libraryArr[i][j]+'\n')
+            node.appendChild(textnode);
+            node.appendChild(document.createNode('br'));
+        }
+        document.querySelector("main").appendChild(node);
     }
 }
-
 
 const addBook = (e)=>{
     e.preventDefault();  //to stop the form submitting
@@ -68,28 +47,15 @@ const addBook = (e)=>{
     document.forms[0].reset(); // to clear the form for the next entries
 }
 
-//save maybe better
 function toggleVisibility(){
     if(form.style.display==="none"){
         form.style.display = "flex";
-        book.style.display = "none"
+        library.style.display = "none"
     }else{
         form.style.display = "none";
-        book.style.display = "flex"
+        library.style.display = "flex"
     }
 }
-
-//produces more problems
-// function toggle(){
-//     if(form.className == "hide"){
-//         form.className = "flex";
-//         book.className = "hide";
-//     }
-//     else{
-//         form.className = "hide"
-//         book.className = "flex";
-//     }
-// }
 
 function addBookForm(){
 
